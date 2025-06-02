76cb8605da3e719ac3308d245b9f596207947e3c
sergiolucasb <156127628+sergiolucasb@users.noreply.github.com>
2025-06-02 22:09:26 +0200

    BACK_patch_usuario: Función patch usuario. Un usuario solo puede editar su propia información. Recalcular calorías automáticamente. (#18)
    Co-authored-by: Sergio Lucas Ballester <sergiolucasballester@gmail.com>

--- backend/src/main/java/com/nutritrack/nutritrack/usuario/mapper/UsuarioMapper.java
+++ backend/src/main/java/com/nutritrack/nutritrack/usuario/mapper/UsuarioMapper.java
@@ -1,13 +1,30 @@
 package com.nutritrack.nutritrack.usuario.mapper;
 
+import com.nutritrack.nutritrack.usuario.api.request.PatchUsuarioRequest;
 import com.nutritrack.nutritrack.usuario.api.request.PostUsuarioRegistro;
 import com.nutritrack.nutritrack.usuario.api.response.UsuarioResponse;
 import com.nutritrack.nutritrack.usuario.entity.Usuario;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(componentModel = "spring")
 public interface UsuarioMapper {
     UsuarioResponse toUsuarioResponse(Usuario usuario);
 
     Usuario toEntity(PostUsuarioRegistro postUsuarioRegistro);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(source = "correo", target = "correo", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(source = "contrasena", target = "contrasena", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(source = "peso", target = "peso", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(source = "altura", target = "altura", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(source = "nivelActividadFisica", target = "nivelActividadFisica", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(source = "objetivoPersonal", target = "objetivoPersonal", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(source = "nombre", target = "nombre", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(source = "apellidos", target = "apellidos", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(source = "caloriasDiarias", target = "caloriasDiarias", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void toPatchUsuario(PatchUsuarioRequest patchUsuarioRequest, @MappingTarget Usuario usuario);
+
 }
